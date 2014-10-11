23f3a7136c80cee3a0fdd0fdb323e3225e593be5
Saksit Jantila <khantee8@gmail.com>
2014-10-11 14:42:09 +0700

    last update today

--- test2_topo.py
+++ test2_topo.py
@@ -17,18 +17,18 @@ def emptyNet():
     net = Mininet( controller=RemoteController )
 
     info( '*** Adding controller\n' )
-    net.addController( 'c0',controller=RemoteController,ip="192.168.56.101",port=6633 )
+    net.addController( 'c1',controller=RemoteController,ip="192.168.56.101",port=6633 )
 
     info( '*** Adding hosts\n' )
-    h1 = net.addHost( 'h1', ip="10.0.0.3" )
-    h2 = net.addHost( 'h2', ip="10.0.0.4" )
+    h3 = net.addHost( 'h3', ip="10.0.0.3" )
+    h4 = net.addHost( 'h3', ip="10.0.0.4" )
 
     info( '*** Adding switch\n' )
-    s3 = net.addSwitch( 's3' )
+    s2 = net.addSwitch( 's2' ,mac="00:00:00:00:00:02" )
 
     info( '*** Creating links\n' )
-    net.addLink( h1, s3 )
-    net.addLink( h2, s3 )
+    net.addLink( h3, s2 )
+    net.addLink( h4, s2 )
 
     info( '*** Starting network\n')
     net.start()
