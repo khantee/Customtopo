a4a68f5ae35aa1540f1cac6b50b2cc0768f4df62
Saksit Jantila <khantee8@gmail.com>
2014-10-11 13:57:31 +0700

    test

--- test1_topo.py
+++ test1_topo.py
@@ -9,6 +9,11 @@ topology enables one to pass in '--topo=mytopo' from the command line.
 """
 
 from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import OVSSwitch, Controller, RemoteController
+from mininet.topolib import TreeTopo
+from mininet.log import setLogLevel
+from mininet.cli import CLI
 
 class MyTopo( Topo ):
     "Simple topology example." 
@@ -23,11 +28,13 @@ class MyTopo( Topo ):
         leftHost = self.addHost( 'h1' , ip='10.0.0.5/8')
         rightHost = self.addHost( 'h2',ip='10.0.0.6/8')
         leftSwitch = self.addSwitch( 's3' )
+        c1 = self.addController('c1',ip='192.168.56.104',port='6633')
         
 
         # Add links
         self.addLink( leftHost, leftSwitch )
         self.addLink( rightHost, leftSwitch )
+        self.addLink( c1, leftSwitch )
 
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
