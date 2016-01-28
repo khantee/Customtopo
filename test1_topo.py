19d4baeeff17b532143bbbf6ad02308d257b8314
Saksit Jantila <Khantee@Saksit-Jantilas-MacBook-Pro.local>
2016-01-28 20:30:56 +0700

    test

--- test1_topo.py
+++ test1_topo.py
@@ -10,36 +10,15 @@ from mininet.node import RemoteController
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 
-def emptyNet():
-
-    "Create an empty network and add nodes to it."
-
-    net = Mininet( controller=RemoteController )
-
-    info( '*** Adding controller\n' )
-    net.addController( 'c1',controller=RemoteController,ip="10.0.0.118",port=6653 )
-
-    info( '*** Adding hosts\n' )
-    h1 = net.addHost( 'h1', ip="10.0.0.1" )
-    h2 = net.addHost( 'h2', ip="10.0.0.2" )
-
-    info( '*** Adding switch\n' )
-    s1 = net.addSwitch( 's1', mac="00:00:00:00:00:01" )
-
-    info( '*** Creating links\n' )
-    net.addLink( h1, s1 )
-    net.addLink( h2, s1 )
-
-    info( '*** Starting network\n')
-    net.addNAT().configDefault()
-    net.start()
-
-    info( '*** Running CLI\n' )
-    CLI( net )
-
-    info( '*** Stopping network' )
-    net.stop()
+def build( self, natIP='10.0.0.254' ):
+        nat1 = self.addNode( 'nat1', cls=NAT, ip=natIP, inNamespace=False )
+        hosts  = [ self.addHost( h, defaultRoute='via ' + natIP )
+                   for h in 'h1', 'h2' ]
+        s1 = self.addSwitch( 's1' )
+       for h in hosts:
+            self.addLink( s1, h )
+            self.addLink( nat1, s1 )
 
 if __name__ == '__main__':
     setLogLevel( 'info' )
-    emptyNet()
+    build()
