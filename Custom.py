8951a4c1cba21b785ef774e24612eb4c8b565359
Saksit Jantila <Khantee@Saksit-Jantilas-MacBook-Pro.local>
2016-02-02 21:22:33 +0700

    no message

--- Custom.py
+++ Custom.py
@@ -9,6 +9,7 @@ topology enables one to pass in '--topo=mytopo' from the command line.
 """
 
 from mininet.topo import Topo
+from mininet.nodelib import NAT
 
 class MyTopo( Topo ):
     "Simple topology example."
@@ -20,14 +21,18 @@ class MyTopo( Topo ):
         Topo.__init__( self )
 
         # Add hosts and switches
+        natIP='10.0.0.254'
         h1 = self.addHost( 'h1', ip = "10.0.0.8"  )
         h2 = self.addHost( 'h2', ip = "10.0.0.9"  )
         s1 = self.addSwitch( 's1', mac = "00:00:00:00:00:01" )
+        nat1 = self.addNode('nat1', cls=NAT, ip=natIP,
+                             inNamespace=False)
        
 
         # Add links
         self.addLink( h1, s1 )
-        self.addLink( h2, s2 )
+        self.addLink( h2, s1 )
+        self.addLink( nat1, s1)
      
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
