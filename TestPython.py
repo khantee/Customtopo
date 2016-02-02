9156db37d303e9864f06ef5c9090fe60fa552fc3
Saksit Jantila <Khantee@Saksit-Jantilas-MacBook-Pro.local>
2016-02-02 22:14:17 +0700

    add First G

---
+++ TestPython.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+for i in range (1,100):
+	if(i==99):
+		print '%s' %"5555"
+		
+	if(i%3==0):
+		print '%d' %(i)
+		continue
+	
+		
---
+++ ToPo_G1.py
@@ -0,0 +1,38 @@
+"""Custom topology example
+
+Two directly connected switches plus a host for each switch:
+
+   host --- switch --- switch --- host
+
+Adding the 'topos' dict with a key/value pair to generate our newly defined
+topology enables one to pass in '--topo=mytopo' from the command line.
+"""
+
+from mininet.topo import Topo
+from mininet.nodelib import NAT
+
+class MyTopo( Topo ):
+    "Simple topology example."
+
+    def __init__( self ):
+        "Create custom topo."
+
+        # Initialize topology
+        Topo.__init__( self )
+
+        # Add hosts and switches
+        natIP='10.0.0.254'
+        s1 = self.addSwitch( 's1', mac = "00:00:00:00:00:01" )
+        nat1 = self.addNode('nat1', cls=NAT, ip=natIP,
+                             inNamespace=False)
+
+        for i  in rang(2,101):
+        host  = self.addHost( 'h%d' % i, ip = "10.0.0.%d" & i,defaultRoute= 'via ' + natIP)
+        # Add links
+        self.addLink( host, s1 )
+        continue
+        
+        self.addLink( nat1, s1)
+
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
