4bdc0a5db0a70d0907e4c438bd1ddcc7bbaf3b86
Saksit Jantila <Khantee@Saksit-Jantilas-MacBook-Pro.local>
2016-02-02 23:35:43 +0700

    fix

--- TestPython.py
+++ TestPython.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 print "asadas"
 print "55556"
-	for i  in range (2,101):
-		print 'h%d' % i
+		for i  in range (2,101):
+			print 'h%d' % i
 
 
 	
--- ToPo_G1.py
+++ ToPo_G1.py
@@ -20,12 +20,13 @@ class MyTopo( Topo ):
 
         # Add hosts and switches
         natIP='10.0.0.254'
-        for i range (2,102):
+        s1 = self.addSwitch( 's1', mac = "00:00:00:00:00:01" )
+        nat1 = self.addNode('nat1', cls=NAT, ip=natIP,inNamespace=False)
+        for i range (2,21):
             h = self.addHost( 'h%d' % i, ip = "10.0.0.%d" % i ,defaultRoute= 'via ' + natIP )
-            s1 = self.addSwitch( 's1', mac = "00:00:00:00:00:01" )
-            nat1 = self.addNode('nat1', cls=NAT, ip=natIP,inNamespace=False)
             self.addLink( h, s1 )
-            self.addLink( nat1, s1)
+
+        self.addLink( nat1, s1)
      
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
